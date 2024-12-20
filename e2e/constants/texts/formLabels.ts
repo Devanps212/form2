d0752ce78fd36edd5e1975e7ca4ca2f81f3392dc
devanps212 <devanps212@gmail.com>
2024-12-20 23:46:31 +0530

    constants setup

---
+++ e2e/constants/texts/formLabels.ts
@@ -0,0 +1,11 @@
+export const FORM_LABELS = {
+    fullName: 'Full name',
+    phNO:'Phone number',
+    multi: 'Multi choice',
+    single: 'Single choice',
+    formTitle: 'Form Title',
+    startFromScratch:'Start from scratchA blank',
+    addNewForm:'Add new form',
+}
+
+
