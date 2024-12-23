e9d8ad6fb1e5cc1e9a314792a585b2bef02186d7
devanps212 <devanps212@gmail.com>
2024-12-23 12:26:10 +0530

    idex file added for grouping all selectors

--- e2e/constants/selectors/index.ts
+++ e2e/constants/selectors/index.ts
@@ -1,5 +1,9 @@
-import { FORM_INPUT_SELECTORS } from "./formInputSelectors";
+import { FORM_INPUT_SELECTORS } from "./formInput";
+import { FORM_PUBLISH_SELECTORS } from "./formPublish";
+import { FORM_HEADER_SELECTORS } from "./formHeader";
 
 export {
-    FORM_INPUT_SELECTORS
+    FORM_INPUT_SELECTORS,
+    FORM_PUBLISH_SELECTORS,
+    FORM_HEADER_SELECTORS
 }
