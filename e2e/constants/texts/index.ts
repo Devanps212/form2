696ba2373d8b519ae8c80e6355bcf5b5674e6ddb
devanps212 <devanps212@gmail.com>
2024-12-23 12:27:26 +0530

    added index file for texts

--- e2e/constants/texts/index.ts
+++ e2e/constants/texts/index.ts
@@ -1,5 +1,9 @@
 import { FORM_LABELS } from "./formLabels";
+import { SUBMISSION_USER_DETAILS } from "./userSubmission";
+import { SUCCESS_MESSAGE } from "./successMessage";
 
 export {
-    FORM_LABELS
+    FORM_LABELS,
+    SUBMISSION_USER_DETAILS,
+    SUCCESS_MESSAGE
 }
